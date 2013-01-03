3a30860418d4425126acfbf68fcbdd457ff8be90
Grant McInnes <grant.mcinnes@eyesopen.ca>
2013-01-03 16:58:29 -0500

    Update adium.rb

--- adium.rb
+++ adium.rb
@@ -1,6 +1,5 @@
 pivotal_workstation_package "Adium" do
-  dmg_name "Adium_1.4.3.dmg"
-  source "http://download.adium.im/Adium_1.4.3.dmg"
-  checksum "5c95d37761b40d0df1e01baeb0bd621b"
+  dmg_name "Adium_1.5.4.dmg"
+  source "http://downloads.sourceforge.net/project/adium/Adium_1.5.4.dmg?r=http%3A%2F%2Fadium.im%2F&ts=1357250249&use_mirror=voxel"
   action :install
 end
