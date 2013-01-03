8fb0ddd94b0ac282674dd81c0a33741ed044838a
Grant McInnes <grant.mcinnes@eyesopen.ca>
2013-01-03 13:57:14 -0800

    Create adium.rb

---
+++ adium.rb
@@ -0,0 +1,6 @@
+pivotal_workstation_package "Adium" do
+  dmg_name "Adium_1.4.3.dmg"
+  source "http://download.adium.im/Adium_1.4.3.dmg"
+  checksum "5c95d37761b40d0df1e01baeb0bd621b"
+  action :install
+end
