8425bcba077ba687b4e6b03ab339439f8999189f
Grant McInnes <grant.mcinnes@eyesopen.ca>
2013-01-03 14:04:17 -0800

    Create metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+maintainer        "Grant McInnes"
+maintainer_email  "grant.mcinnes@eyesopen.ca"
+license           "MIT"
+description       "Configure frequently-used tools for an OSX workstation"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "1.0.0"
+supports          "mac_os_x"
+depends           "dmg"
