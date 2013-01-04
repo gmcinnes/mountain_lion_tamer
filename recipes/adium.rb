330bcc6498e2cbbe0e717a94c94982085690d03c
Grant McInnes <grant.mcinnes@eyesopen.ca>
2013-01-03 21:14:24 -0500

    Update recipes/adium.rb

--- recipes/adium.rb
+++ recipes/adium.rb
@@ -1,5 +1,5 @@
-pivotal_workstation_package "Adium" do
-  dmg_name "Adium_1.5.4.dmg"
+dmg_package "Adium" do
+  volumes_dir "Adium 1.5.4"
   source "http://downloads.sourceforge.net/project/adium/Adium_1.5.4.dmg?r=http%3A%2F%2Fadium.im%2F&ts=1357250249&use_mirror=voxel"
   action :install
 end
